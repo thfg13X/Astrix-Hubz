d2ca29bbeef4e46352f5aed771ecac88fa7cdfa8
thfg13X <etherealicuniversal@gmail.com>
2026-02-24 18:44:26 +0100

    Update Loader.lua

--- Loader.lua
+++ Loader.lua
@@ -1,6 +1,11 @@
 script_key = "trial"
 
-loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/2872ef09b36cafac79a1420bacc1b75587ed50b426e3b135b3710a872bdd83d2.lua"))()
+pcall(function()
+    loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/2872ef09b36cafac79a1420bacc1b75587ed50b426e3b135b3710a872bdd83d2.lua"))()
+end)
+
 script_key = "trial"
 
-loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/f2aa9049c8fd2327d721dc75bb7655dc217de6ea5dc7760ca94597b458a3df2b.lua"))()
+pcall(function()
+    loadstring(game:HttpGet("https://api.getpolsec.com/scripts/hosted/f2aa9049c8fd2327d721dc75bb7655dc217de6ea5dc7760ca94597b458a3df2b.lua"))()
+end)
